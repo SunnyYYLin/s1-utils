0e9bb87f930e7b4b2d443c6a416937be9944f8ac
SunnyYYLin <sunnylinyy@outlook.com>
2025-05-07 00:32:44 +0800

    优化OBS客户端实例创建，简化环境变量获取方式，移除不必要的注释

--- test.py
+++ test.py
@@ -1,19 +1,11 @@
 # 引入模块
 import os
 from obs import ObsClient
-from collections import namedtuple
 
-# 推荐通过环境变量获取AKSK，这里也可以使用其他外部引入方式传入，如果使用硬编码可能会存在泄露风险。
-# 您可以登录访问管理控制台获取访问密钥AK/SK，获取方式请参见https://support.huaweicloud.com/usermanual-ca/ca_01_0003.html。
+OBS_SERVER = "obs.cn-east-4.myhuaweicloud.com"
 ak = os.getenv("HuaweiCloudAccessKeyID")
 sk = os.getenv("HuaweiCloudSecretAccessKey")
-# 【可选】如果使用临时AKSK和SecurityToken访问OBS，则同样推荐通过环境变量获取
-security_token = os.getenv("SecurityToken")
-#  server填写Bucket对应的Endpoint, 这里以华北-北京四为例，其他地区请按实际情况填写。
-server = "obs.cn-east-4.myhuaweicloud.com"
-# 创建obsClient实例
-# 如果使用临时AKSK和SecurityToken访问OBS，需要在创建实例时通过security_token参数指定securityToken值
-obsClient = ObsClient(access_key_id=ak, secret_access_key=sk, server=server)
+obsClient = ObsClient(access_key_id=ak, secret_access_key=sk, server=OBS_SERVER)
 # 使用访问OBS
 log_dir = '/wenge/log'
 
@@ -34,7 +26,7 @@ def get_log_objects(log_dir: str, client: ObsClient, suffix='.log') -> list[dict
     dir += '/'
     print(bucket, dir)
     resp = obsClient.listObjects(bucket, prefix=dir, delimiter="/")
-    print(resp)
+    print(len(resp.body.contents))
     assert resp.status < 300, f"listObjects failed: {resp.status} {resp.body}"
     return [obj for obj in resp.body.contents if obj.key.endswith(suffix)]
 
